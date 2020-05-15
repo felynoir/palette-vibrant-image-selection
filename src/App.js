d0b0590d47f89db2336edc3d39fa8dfa1f6849b5
dragonfly161518 <por.nattapat.01@gmail.com>
2020-05-15 19:29:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as Vibrant from "node-vibrant";
 
 const App = () => {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  const url =
+    "https://i.scdn.co/image/ab67616d00001e026ab5fc4e1dd227bef3100280";
+  const [state, setState] = useState({});
+  useEffect(() => {
+    Vibrant.from(url)
+      .getPalette()
+      .then((palette) => setState(palette));
+  }, []);
+  return (
+    <div>
+      <div>
+        <img src={url} />
       </div>
-    );
-}
+      {state &&
+        Object.keys(state).map((key, ind) => {
+          console.log(key);
+          console.log(state[key]);
+          return (
+            <div
+              key={ind}
+              style={{ display: "inline-block", marginRight: "40px" }}
+            >
+              {key}
+              <div
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  backgroundColor: "#000",
+                  margin: "10px",
+                }}
+              ></div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 
 export default App;
